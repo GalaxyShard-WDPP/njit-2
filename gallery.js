b2fbc8ee0395d30790d98a15553718252f3cc9aa
GalaxyShard-WDPP <524dadragna@frhsd.com>
2024-02-02 14:43:49 -0500

    styling fixes, refactored js

--- gallery.js
+++ gallery.js
@@ -1,22 +1,29 @@
 // example code from mr doob : https://mrdoob.com/lab/javascript/requestanimationframe/
-let mLastFrameTime = 0;
-let mWaitTime = 5000; //time in ms
+let lastFrameTime = 0;
+let waitTime = 5000; //time in ms
 
+function resetTime() {
+	lastFrameTime = new Date().getTime();
+}
 function animate() {
 	requestAnimationFrame(animate); // requestAnimationFrame has existed in every standard browser since 2016
 
 	let currentTime = new Date().getTime();
-	if (mLastFrameTime === 0) {
-		mLastFrameTime = currentTime;
+	if (lastFrameTime === 0) {
+		lastFrameTime = currentTime;
 	}
-	if ((currentTime - mLastFrameTime) > mWaitTime) {
+	if ((currentTime - lastFrameTime) > waitTime) {
 		swapPhoto();
-		mLastFrameTime = currentTime;
+		lastFrameTime = currentTime;
 	}
 }
 animate();
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+function mod(n, m) {
+	return ((n % m) + m) % m;
+}
+
 let currentIndex = 0;
 
 let images = [];
@@ -27,6 +34,13 @@ function swapPhoto() {
 	if (images.length == 0) {
 		return;
 	}
+	updatePhoto();
+	currentIndex = mod(currentIndex+1, images.length);
+}
+function updatePhoto() {
+	if (images.length == 0) {
+		return;
+	}
 	let image = images[currentIndex];
 
 	$(".location").text("Location: " + image.location);
@@ -34,20 +48,7 @@ function swapPhoto() {
 	$(".date").text("Date: " + image.captureDate);
 	$("#photo").attr("src", image.photo);
 
-	currentIndex += 1;
-	currentIndex %= images.length;
 }
-
-
-//You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
-//@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
-function makeGalleryImageOnloadCallback(galleryImage) {
-	return function(e) {
-		galleryImage.img = e.target;
-		images.push(galleryImage);
-	}
-}
-
 
 
 class GalleryImage {
@@ -62,23 +63,55 @@ class GalleryImage {
 
 function fetchJson() {
 	// could just use `fetch` which has been widely supported for almost a decade
-	// fetch(jsonUrl).then(data => data.json()).then(json => {}).catch(e => console.log(e));
-	let request = new XMLHttpRequest();
-	request.responseType = "json";
-	request.addEventListener("load", e => {
-		let galleryJson = request.response; // `response` parses json automatically (because responseType is set to "json"), unlike the deprecated `responseText`
-
-		for (let image of galleryJson.images) {
+	fetch(jsonUrl).then(data => data.json()).then(json => {
+		for (let image of json.images) {
 			images.push(new GalleryImage(image.imgLocation, image.description, image.date, image.imgPath));
 		}
-	});
-	request.open("GET", jsonUrl);
-	request.send();
+		resetTime();
+		updatePhoto();
+	}).catch(e => console.log(e));
+	
+	// alternatively, XMLHttpRequest
+	// let request = new XMLHttpRequest();
+	// request.responseType = "json";
+	// request.addEventListener("load", e => {
+	// 	   let galleryJson = request.response; // `response` parses json automatically (because responseType is set to "json"), unlike the deprecated `responseText`
+
+	// 	    for (let image of galleryJson.images) {
+	// 			images.push(new GalleryImage(image.imgLocation, image.description, image.date, image.imgPath));
+	// 		}
+	//  	resetTime();
+	//  	updatePhoto();
+	// });
+	// request.open("GET", jsonUrl);
+	// request.send();
 }
 
 // no need for .ready or onload, JavaScript modules are deferred
 
 // This initially hides the photos' metadata information
 $('.details').eq(0).hide();
+
+$(".moreIndicator").on("click", e => {
+	$(".details").slideToggle();
+	$(e.currentTarget).toggleClass("rot90");
+	$(e.currentTarget).toggleClass("rot270");
+});
+$("#nextPhoto").on("click", e => {
+	if (images.length == 0) {
+		return;
+	}
+	resetTime();
+	currentIndex = mod(currentIndex+1, images.length);
+	updatePhoto();
+});
+$("#prevPhoto").on("click", e => {
+	if (images.length == 0) {
+		return;
+	}
+	resetTime();
+	currentIndex = mod(currentIndex-1, images.length);
+	updatePhoto();
+});
 
 fetchJson();
